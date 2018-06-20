43afcdbfbc9da6d4807557bf7343a30c134ec7f7
Nagarajan N D <37927458+nagarajannd@users.noreply.github.com>
2018-06-20 05:49:43 -0400

    Update AssemblyInfo.cs

--- WorkflowManagementTool - OS/WorkflowManagementTool/Properties/AssemblyInfo.cs
+++ WorkflowManagementTool - OS/WorkflowManagementTool/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("WorkflowManagementTool")]
 [assembly: AssemblyDescription("Workflow Management Tool")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hinduja Global Solutions")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WorkflowManagementTool")]
-[assembly: AssemblyCopyright("Copyright © Hinduja Global Solutions. 2018")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
